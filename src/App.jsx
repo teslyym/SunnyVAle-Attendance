3d65d9e4875794e249fcd5e9d38edadf58d43326
Teslim <steslimolayiwola@gmail.com>
2024-08-23 12:13:19 +0100

    addition of header

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,23 @@ import "./App.css";
 
 function App() {
   return;
-  <></>;
+  <>
+  <body>
+    <header>
+       <div class="head">
+        <div class="head1"><h1>SUNNY<br>Vale</h1></div>
+        <div class="navlist">
+                <a href="Home">Home</a>
+                <a href="About Us">About Us</a>
+                <a href="Gallery">Gallery</a>
+                <a href="Contact Us">Contact Us</a>
+        </div>
+        <div class="head3">
+            <button id="but1">Blog/News</button>
+        </div>
+       </div>
+      </header>
+    </body></>;
 }
 
 export default App;
