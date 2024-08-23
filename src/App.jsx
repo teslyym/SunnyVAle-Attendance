c13a1a947e185748a0dbb0e60c58702501fedde1
Teslim <steslimolayiwola@gmail.com>
2024-08-23 12:40:59 +0100

    bug fix

--- src/App.jsx
+++ src/App.jsx
@@ -3,176 +3,248 @@ import "./App.css";
 function App() {
   return (
     <>
-      <body>
-        <header>
-          <div class="head">
-            <div class="head1">
-              <h1>
-                SUNNY <br /> Vale
-              </h1>
+      <header>
+        <div className="head">
+          <div className="head1">
+            <h1>
+              SUNNY <br /> Vale
+            </h1>
+          </div>
+          <div className="navlist">
+            <a href="Home">Home</a>
+            <a href="About Us">About Us</a>
+            <a href="Gallery">Gallery</a>
+            <a href="Contact Us">Contact Us</a>
+          </div>
+          <div className="head3">
+            <button id="but1">Blog/News</button>
+          </div>
+        </div>
+      </header>
+      <section>
+        <div className="main">
+          <div className="main1">
+            <h1 id="main2">DELIVERING PREMIUM SERVICES</h1>
+            <p id="main3">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore
+            </p>
+          </div>
+          <div id="main4">
+            <img src="res/Frame 7.png" alt="Houses in SunnyVale" />
+          </div>
+        </div>
+      </section>
+      <section>
+        <div className="mid">
+          <div className="mid1">
+            <h1 id="mid2">
+              EXPERIENCE THE ALLURE OF <br /> SUNNYVALE ESTATE
+            </h1>
+            <div className="midd">
+              <p id="mid3">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                <br />
+                sed do eiusmod tempor incididunt ut labore et dolore
+                <br />
+                magna aliqua. Ut enim ad minim veniam, quis nostrud
+                <br />
+                exercitation ullamco laboris nisi ut aliquip ex ea
+                <br />
+                commodo consequat. Duis aute irure dolor in reprehenderit in
+                <br />
+                voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+                <br />
+                Excepteur sint occaecat cupidatat non proident, sunt in
+                <br />
+                culpa qui officia deserunt mollit anim id est laborum
+              </p>
+              <div id="mid4">
+                <img src="res/Frame 34.png" alt="Image" />
+              </div>
             </div>
-            <div class="navlist">
-              <a href="Home">Home</a>
-              <a href="About Us">About Us</a>
-              <a href="Gallery">Gallery</a>
-              <a href="Contact Us">Contact Us</a>
-            </div>
-            <div class="head3">
-              <button id="but1">Blog/News</button>
+            <div id="mid5">
+              <img src="res/Union.png" alt="A Right Arrow" />
             </div>
           </div>
-        </header>
-        <section>
-        
-          <div class="main">
-            <div class="main1">
-              <h1 id="main2">DELIVERING PREMIUM SERVICES</h1>
-              <p id="main3">
-                Lorem ipsum dolor sit amet,consectetur adipiscing elit,sed do
-                eisumond tempor incididunt ut labore
+        </div>
+      </section>
+      <section>
+        <div className="nearfooter">
+          <div className="nearfooter1">
+            <h1 id="nearfooter2">
+              UNVEIL THE HIGHLIGHTS <br /> OF OUR NEIGHBOURHOOD
+            </h1>
+            <p id="nearfooter3">
+              Lorem ipsum dolor sit amet, ghjnjnkm
+              <br />
+              consectetur adipiscing elit, sed donoti
+              <br />
+              eiusmod tempor incididunt ut labore
+              <br />
+              et dolore magna aliqua. Ut enim ad
+            </p>
+          </div>
+          <div className="nearfooter4">
+            <div className="nearfoot1">
+              <div id="nearfoot1b">
+                <div className="nearfoot2">
+                  <div id="nearfoot4">
+                    <div id="nearfoott4">
+                      <h1 id="nearfoot4a">24/7 SECURITY</h1>
+                      <div id="nearfoot4b">
+                        <img src="res/Rectangle 1.png" alt="Security Officer" />
+                      </div>
+                    </div>
+                    <div className="nearfoot3">
+                      <p id="nearfoot3a">
+                        Lorem ipsum dolor sit amet,
+                        <br />
+                        ghjnjnkm consectetur hmbhgv
+                        <br />
+                        adipiscing elit, sed donoti
+                        <br />
+                        eiusmod tempor incididunt ut labore
+                        <br />
+                        et dolore magna aliqua.
+                      </p>
+                      <div>
+                        <button id="but2">View More</button>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="nearfoot5">
+                    <h1 id="nearfoot5a">GYMS & HEALTH</h1>
+                    <div id="nearfoot5b">
+                      <img src="res/Frame 29.png" alt="Gym Equipments" />
+                    </div>
+                  </div>
+                  <div className="nearfoot6">
+                    <h1 id="nearfoot6a">SUPERMARKETS</h1>
+                    <div id="nearfoot6b">
+                      <img src="res/Rectangle 2.png" alt="Supermarket" />
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section>
+        <div className="basfooter">
+          <div className="basfooter1">
+            <h1 id="basfooter2">
+              VISUAL TOUR OF <br /> OUR SUNNYVALE ESTATE
+            </h1>
+            <p id="basfooter3">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+              <br />
+              do eiusmod tempor incididunt ut labore et dolore magna
+              <br />
+              aliqua. Ut enim ad minim veniam, quis nostrud exercitation
+              <br />
+              ullamco laboris nisi ut aliquip ex ea commodo consequat.
+              <br />
+              Duis aute irure dolor in reprehenderit in voluptate velit esse
+              <br />
+              cillum dolore eu fugiat nulla pariatur. Excepteur sint
+              <br />
+              occaecat cupidatat non proident, sunt in culpa qui officia
+              <br />
+              deserunt mollit anim id est laborum.
+            </p>
+          </div>
+          <div id="basfooter4">
+            <img src="res/Frame 50.png" alt="Tour Pictures" />
+          </div>
+        </div>
+      </section>
+      <section>
+        <div className="finfooter">
+          <div className="finfooter1"></div>
+          <div className="finfooter2">
+            <h1 id="finfooter3">
+              LET'S START <br /> A CONVERSATION
+            </h1>
+            <p id="finfooter4">
+              Lorem ipsum dolor sit amet, ghjnjnkm
+              <br />
+              consectetur adipiscing elit, sed donoti
+              <br />
+              eiusmod tempor incididunt ut labore
+              <br />
+              et dolore magna alique.Ut enim ad
+            </p>
+          </div>
+          <div className="finfooter5">
+            <div>
+              <h1 id="finfooter5a">Send an Inquiry</h1>
+            </div>
+            <div className="finfooter6">
+              <input type="text" id="name" name="Name" placeholder="Name" />
+              <br />
+              <input
+                type="text"
+                id="emailaddress"
+                name="Email"
+                placeholder="Email Address"
+              />
+              <br />
+              <input
+                type="text"
+                id="phonenumber"
+                name="Phone"
+                placeholder="Phone Number"
+              />
+              <br />
+              <input
+                type="text"
+                id="message"
+                name="Message"
+                placeholder="Message"
+              />
+              <br />
+            </div>
+            <div className="finfooter7">
+              <button id="but3">Submit Inquiry</button>
+            </div>
+          </div>
+        </div>
+      </section>
+      <footer>
+        <div className="footer">
+          <div className="footer1">
+            <h1 id="footer2">SunnyVale Estate</h1>
+            <p id="footer3">
+              Privacy Policy
+              <br />
+              Terms and Conditions
+            </p>
+          </div>
+          <div className="footer4">
+            <div id="footer5">
+              <div>
+                <img src="res/devicon_twitter.png" alt="X logo" />
+              </div>
+              <div>
+                <img src="res/logos_whatsapp-icon.png" alt="Whatsapp logo" />
+              </div>
+              <div>
+                <img src="res/Group.png" alt="Instagram logo" />
+              </div>
+              <div>
+                <img src="res/Vector (1).png" alt="Facebook logo" />
+              </div>
+            </div>
+            <div id="footer6">
+              <p id="footer7">
+                © 2024 BEU SunnyVale Estate All Rights Reserved
               </p>
             </div>
-            <div id="main4">
-              <img src="res/Frame 7.png" alt="Houses in SunnyVale" />
-            </div>
           </div>
-        </section>
-        <section/>
-        <div class="mid">
-            <div class="mid1"/>
-                <div>  <h1 id="mid2">EXPERIENCE THE ALLURE OF <br/> SUNNYVALE ESTATE</h1></div>
-                <div class="midd">
-                <p id="mid3">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit,<br/>
-                     sed do eiusmod tempor incididunt ut labore et dolore <br/>
-                     magna aliqua. Ut enim ad minim veniam, quis nostrud <br/>
-                    exercitation ullamco laboris nisi ut aliquip ex ea <br/>
-                     commodo consequat. Duis aute irure dolor in reprehenderit in <br/>
-                    voluptate velit esse cillum dolore eu fugiat nulla pariatur.  <br/>
-                     Excepteur sint occaecat cupidatat non proident, sunt in <br/>
-                     culpa qui officia deserunt mollit anim id est laborum
-                </p>
-                <div id="mid4"><img src="res/Frame 34.png" alt="Image"/></div>
-                </div>
-                 <div id="mid5"><img src="res/Union.png" alt=" A Right Arrow"/></div> 
-            </div>
-        <section />
-        <section>
-        <div class="nearfooter">
-            <div class="nearfooter1">
-                <h1 id="nearfooter2">UNVEIL THE HIGHLIGHTS <br>   OF OUR NEIGHBOURHOOD</h1>
-                <p id="nearfooter3">Lorem ipsum dolor sit amet, ghjnjnkm <br>
-                   consectetur adipiscing elit, sed donoti <br>
-                   eiusmod tempor incididunt ut labore <br>
-                   et dolore magna aliqua. Ut enim ad </p>
-            </div>
-            <div class="nearfooter4">
-                <!-- <div id="nearfoot1"><img src="res/Frame 18.png" alt="24/7 SECURITY"></div> -->
-                <!-- <div id="nearfoot2"><img src="res/Frame 20.png" alt="GYMS AND HEALTH"></div>
-                <div id="nearfoot3"><img src="res/Frame 26.png" alt="SUPERMARKETS"></div> -->
-                <div class="nearfoot1">
-                    <!-- <h1 id="nearfoot1a">24/7 SECURITY</h1> -->
-                    <div id="nearfoot1b">
-                     <div class="nearfoot2">
-                        <!-- <h1 id="nearfoot1a">24/7 SECURITY</h1> -->
-                        <div id="nearfoot4">
-                            <div id="nearfoott4">
-                            <h1 id="nearfoot4a">24/7 SECURITY</h1>
-                            <div id="nearfoot4b"><img src="res/Rectangle 1.png" alt="Security Officer"></div>
-                            </div>
-                        <div class="nearfoot3">
-                            <p id="nearfoot3a">Lorem ipsum dolor sit amet,<br>
-                               ghjnjnkm consectetur hmbhgv <br>
-                               adipiscing elit, sed donoti <br>
-                               eiusmod tempor incididunt ut labore <br>
-                               et dolore magna aliqua.</p>
-                               <div> 
-                                <button id="but2">View More</button>
-                               </div>
-                        </div>
-                        </div>
-                        <div class="nearfoot5" >
-                            <h1 id="nearfoot5a">GYMS & HEALTH</h1>
-                            <div id="nearfoot5b"><img src="res/Frame 29.png" alt="Gym Equipments"></div>
-                        </div>
-                        <div class="nearfoot6">
-                            <h1 id="nearfoot6a">SUPERMARKETS</h1>
-                            <div id="nearfoot6b"><img src="res/Rectangle 2.png" alt="Supermarket"></div>
-                        </div>
-                     </div>
-                    </div>
-                </div>
-            </div>
         </div>
-                  </section>
-                  <section>
-        <div class="basfooter">
-            <div class="basfooter1">
-                <h1 id="basfooter2">VISUAL TOUR OF <br> OUR SUNNYVALE ESTATE</h1>
-                <p id="basfooter3">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed <br>
-                   do eiusmod tempor incididunt ut labore et dolore magna <br>
-                   aliqua. Ut enim ad minim veniam, quis nostrud exercitation <br>
-                   ullamco laboris nisi ut aliquip ex ea commodo consequat. <br>
-                   Duis aute irure dolor in reprehenderit in voluptate velit esse <br>
-                   cillum dolore eu fugiat nulla pariatur. Excepteur sint <br>
-                   occaecat cupidatat non proident, sunt in culpa qui officia<br>
-                   deserunt mollit anim id est laborum.</p>
-            </div>
-            <div id="basfooter4"><img src="res/Frame 50.png" alt="Tour Pictures"></div>
-        </div>
-                              </section>
-                              <section>
-        <div class="finfooter">
-        <div class="finfooter1">
-            <!-- <div id="finfooter1"><img src="res/Frame 55.png" alt="Login Page"></div> -->
-        </div>
-        <div class="finfooter2">
-            <h1 id="finfooter3">LET'S START <br> A CONVERSATION</h1>
-            <p id="finfooter4">Lorem ipsum dolor sit amet, ghjnjnkm <br>
-               consectetur adipiscing elit, sed donoti <br>
-               eiusmod tempor incididunt ut labore <br>
-               et dolore magna alique.Ut enim ad</p>
-        </div>
-        <div class="finfooter5">
-            <div>
-                <h1 id="finfooter5a">Send an Inquiry</h1>
-            </div>
-            <div class="finfooter6">
-                <input type="text" id="name" name="Name" placeholder="Name"><br>
-                <input type="text" id="emailaddress" name="Name" placeholder="Email Address"><br>
-                <input type="text" id="phonenumber" name="Name" placeholder="Phone Number"><br>
-                <input type="text" id="message" name="Name" placeholder="Message"><br>
-            </div>
-            <div class="finfooter7">
-                <button id="but3">Submit Inquiry</button>
-            </div>
-        </div>
-        </div>
-                                              </section>
-                                               <footer>
-        <div class="footer">
-            <div class="footer1">
-                <h1 id="footer2">SunnyVale Estate</h1>
-                <p id="footer3">Privacy Policy
-                    <br>
-                   Terms and Conditions
-                </p>
-            </div>
-            <div class="footer4">
-                <div id="footer5">
-                    <div><img src="res/devicon_twitter.png" alt="X logo"></div>
-                    <div><img src="res/logos_whatsapp-icon.png" alt="Whatsapp logo"></div>
-                    <div><img src="res/Group.png" alt="Instagram logo"></div>
-                    <div><img src="res/Vector (1).png" alt="Facebook logo"></div>
-                </div>
-                <div id="footer6">
-                    <p id="footer7">© 2024 BEU SunnyVale Estate All Rights Reserved</p>
-                </div>
-            </div>
-        </div>
-    </footer>
-
-      <body/>
+      </footer>
     </>
   );
 }
