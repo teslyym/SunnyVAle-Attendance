7f17cf0fbb3ca3a59ebec027f31037f2d8c0eca4
Teslim <steslimolayiwola@gmail.com>
2024-08-23 12:34:01 +0100

    section2 and img

--- src/App.jsx
+++ src/App.jsx
@@ -23,6 +23,7 @@ function App() {
           </div>
         </header>
         <section>
+        
           <div class="main">
             <div class="main1">
               <h1 id="main2">DELIVERING PREMIUM SERVICES</h1>
@@ -36,7 +37,27 @@ function App() {
             </div>
           </div>
         </section>
-      </body>
+        <section/>
+        <div class="mid">
+            <div class="mid1"/>
+                <div>  <h1 id="mid2">EXPERIENCE THE ALLURE OF <br/> SUNNYVALE ESTATE</h1></div>
+                <div class="midd">
+                <p id="mid3">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit,<br/>
+                     sed do eiusmod tempor incididunt ut labore et dolore <br/>
+                     magna aliqua. Ut enim ad minim veniam, quis nostrud <br/>
+                    exercitation ullamco laboris nisi ut aliquip ex ea <br/>
+                     commodo consequat. Duis aute irure dolor in reprehenderit in <br/>
+                    voluptate velit esse cillum dolore eu fugiat nulla pariatur.  <br/>
+                     Excepteur sint occaecat cupidatat non proident, sunt in <br/>
+                     culpa qui officia deserunt mollit anim id est laborum
+                </p>
+                <div id="mid4"><img src="res/Frame 34.png" alt="Image"/></div>
+                </div>
+                 <div id="mid5"><img src="res/Union.png" alt=" A Right Arrow"/></div> 
+            </div>
+        <section/>
+      <body/>
     </>
   );
 }
