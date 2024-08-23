be45028ddc5c05e9beb482e59728340972e26d4b
Teslim <steslimolayiwola@gmail.com>
2024-08-23 12:27:25 +0100

    section1 and img

--- src/App.jsx
+++ src/App.jsx
@@ -22,6 +22,20 @@ function App() {
             </div>
           </div>
         </header>
+        <section>
+          <div class="main">
+            <div class="main1">
+              <h1 id="main2">DELIVERING PREMIUM SERVICES</h1>
+              <p id="main3">
+                Lorem ipsum dolor sit amet,consectetur adipiscing elit,sed do
+                eisumond tempor incididunt ut labore
+              </p>
+            </div>
+            <div id="main4">
+              <img src="res/Frame 7.png" alt="Houses in SunnyVale" />
+            </div>
+          </div>
+        </section>
       </body>
     </>
   );
